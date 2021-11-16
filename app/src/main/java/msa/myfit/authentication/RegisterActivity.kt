5364e43317d7ef336d95390c5312dcff455a8306
Petrified0110 <[petra.horvath99@gmail.com]>
2021-11-16 15:00:21 +0200

    Remove OAuth2 stuff from register activity

--- app/src/main/java/msa/myfit/authentication/RegisterActivity.kt
+++ app/src/main/java/msa/myfit/authentication/RegisterActivity.kt
@@ -2,12 +2,15 @@ package msa.myfit.authentication
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.AuthResult
@@ -79,14 +82,17 @@ class RegisterActivity : AppCompatActivity() {
                                         Toast.LENGTH_SHORT
                                     ).show()
 
-                                    val intent = Intent(this@RegisterActivity, MainActivity::class.java)
-                                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                    val intent = Intent(
+                                        this@RegisterActivity,
+                                        MainActivity::class.java
+                                    )
+                                    intent.flags =
+                                        Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                                     intent.putExtra("user_id", firebaseUser.uid)
                                     intent.putExtra("email_id", email)
                                     startActivity(intent)
                                     finish()
-                                }
-                                else{
+                                } else {
                                     Toast.makeText(
                                         this@RegisterActivity,
                                         task.exception!!.message.toString(),
@@ -100,6 +106,4 @@ class RegisterActivity : AppCompatActivity() {
             }
         }
     }
-
-
 }
