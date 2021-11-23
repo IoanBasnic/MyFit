b98636a02f3ae2fdfd406fd5dbbef811ce02ae18
Petrified0110 <[petra.horvath99@gmail.com]>
2021-11-23 20:40:33 +0200

    Create logic for adding calorie intake, retrive all calorie intakes for a user, for a day, display it in a table

--- app/src/main/java/msa/myfit/fragment/HomeFragment.kt
+++ app/src/main/java/msa/myfit/fragment/HomeFragment.kt
@@ -5,9 +5,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
+import msa.myfit.MainActivity
 import msa.myfit.R
 
 
@@ -21,11 +23,12 @@ private const val ARG_PARAM2 = "param2"
  * Use the [HomeFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class HomeFragment : Fragment() {
+class HomeFragment(mainActivity: MainActivity) : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
+    private val mainActivity = mainActivity
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -64,8 +67,8 @@ class HomeFragment : Fragment() {
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
+        fun newInstance(param1: String, param2: String, mainActivity: MainActivity) =
+            HomeFragment(mainActivity).apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
@@ -82,7 +85,7 @@ class HomeFragment : Fragment() {
             var fragment: Fragment? = null
             when (view.id) {
                 R.id.calories_eaten  -> {
-                    fragment = CaloriesEatenFragment()
+                    fragment = CaloriesEatenFragment(mainActivity)
                     replaceFragment(fragment)
                 }
             }
